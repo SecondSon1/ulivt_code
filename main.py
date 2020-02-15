9ba22b7781762dc94a071c86f5577c92ffe4c78a
SecondSon1 <bulatunusov100@gmail.com>
2020-02-15 13:42:44 +0400

    Liza_B

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 n = int(input())
-print(sum([int(t) for t in input().split()]) // n)
+cars_places = dict()
+ans = []
+for i in range(n):
+    cmd = input().split()
+    if len(cmd) == 5:
+        # drove into place
+        cars_places[cmd[0]] = cmd[4]
+    elif cmd[1] == "drove":
+        # drove out
+        del cars_places[cmd[0]]
+    else:
+        # look at
+        res = cars_places.get(cmd[2], None)
+        ans.append('Not here' if res is None else res)
+
+print(*ans, sep='\n')
