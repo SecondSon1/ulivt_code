845d5cc6576b84b3c8558fd625d8ce5622294808
SecondSon1 <bulatunusov100@gmail.com>
2020-02-15 13:27:03 +0400

    Liza_A

--- main.py
+++ main.py
@@ -1,21 +1,2 @@
-def root(p, c=0):
-    global a, b
-    if b[p] == -1:
-        a[p] += c
-        return p
-    b[p] = root(b[p], a[p] + c)
-    a[p] = 0
-    return b[p]
-
-
 n = int(input())
-a = [int(i) for i in input().split()]
-b = [int(i) - 1 for i in input().split()]
-unref = set([i for i in range(n)])
-for i in range(n):
-    if b[i] != -1:
-        unref.remove(b[i])
-unref = list(unref)
-for point in unref:
-    root(point)
-print(*a)
+print(sum([int(t) for t in input().split()]) // n)
